0728965b4cef814fb3b69a317cc7b9e5ff918bd7
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-02-12 20:50:03 +0530

    final responsive for about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,8 +19,8 @@ export const About = () => {
                         />
                     </div>
                     <div>
-                        <h2 className="text-3xl font-bold text-gray-800 mb-4">Natarajan and co</h2>
-                        <p className="text-gray-600 leading-relaxed">
+                        <h2 className="md:text-3xl text-xl font-bold text-gray-800 mb-4">Natarajan and co</h2>
+                        <p className="text-gray-600 leading-relaxed max-md:text-sm">
                         With 21 years of expertise as a Proud <strong> UltraTech</strong> partner, we specialize in providing high-quality construction products. Committed to excellence, we offer a complete range of materials to support your construction needs.
                         </p>
                     </div>
